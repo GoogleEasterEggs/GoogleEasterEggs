332190d2fde08e10764e580ab458d56e83b62bb5
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2024-08-18 23:38:04 +0200

    add

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -3,8 +3,18 @@ const GoogleSecrets = [
         name: "Christmas",
         description: "Opens up Santa Tracker when you click on the Gift Icon.",
         feelingLucky: false,
-        HowtoLink: "https://googleeastereggs.github.io/christmas"
-    }
+        howtoLink: "https://googleeastereggs.github.io/christmas",
+        link: "https://www.google.com/search?q=christmas",
+        tags: ['mobile', 'pc']
+    },
+    {
+        name: "Do a barrel roll",
+        description: "The Google Page spins around when searched up.",
+        feelingLucky: false,
+        howtoLink: false,
+        link: "https://www.google.com/search?q=do+a+barrel+roll",
+        tags: ['mobile', 'pc'],
+    },
 ];
 
 export default GoogleSecrets
